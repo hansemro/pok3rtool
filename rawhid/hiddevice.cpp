hansemro/pok3rtool

commit 182e5cee8bf0d04dd6d8db5920214e1e5146bf0a

[HACK] proto_holtek: fix writeFirmware operation
Hacks a new control transfer operation to hid and hiddevice so that it
can be used to wait for the completion of individual write operations in
proto_holtek.

This targets Linux with LibUSB-1.0 for now. Plan is to support macOS and
Windows as LibUSB-1.0 becomes standard backend.

With this, writeFirmware is confirmed to flash properly albeit at a less
optimal pace. Future plan involves writing larger chunks then waiting
for the chunk to be written before moving to the next chunk.

rawhid/hiddevice.cpp
@@ -102,6 +102,24 @@ bool HIDDevice::recv(ZBinary &data){
     return true;
 }
 
+int HIDDevice::xferControl(zu8 bmRequest, zu8 bRequest, zu16 wValue, zu16 wIndex, ZBinary &data){
+    if(!isOpen())
+        return -1;
+    if(data.size() == 0)
+        return -1;
+
+    ZClock clock;
+    int ret;
+    do {
+        ret = rawhid_xfer_control(hid, bmRequest, bRequest, wValue, wIndex, data.raw(), data.size(), RECV_TIMEOUT);
+    } while(ret == 0 && !clock.passedMs(RECV_TIMEOUT_MAX));
+    if (ret < 0) {
+        ELOG("control recv error: " << ret << ": " << libusb_strerror(ret));
+        return ret;
+    }
+    return ret;
+}
+
 ZArray<ZPointer<HIDDevice> > HIDDevice::openAll(zu16 vid, zu16 pid, zu16 usage_page, zu16 usage){
     ZArray<ZPointer<HIDDevice>> devs;
 #if LIBCHAOS_PLATFORM == LIBCHAOS_PLATFORM_MACOSX

CMakeLists.txt
@@ -60,7 +60,7 @@ add_library(pok3rlib STATIC ${POK3RLIB_SOURCES})
 set_property(TARGET pok3rlib PROPERTY CXX_STANDARD 11)
 target_link_libraries(pok3rlib chaos-static rawhid nlohmann_json ${LIBUSB_1_LIBRARIES})
 #set_property(TARGET chaos-static PROPERTY POSITION_INDEPENDENT_CODE ON)
-target_include_directories(pok3rlib PRIVATE ${LIBUSB_1_INCLUDE_DIRECTORIES} ${CMAKE_CURRENT_BINARY_DIR})
+target_include_directories(pok3rlib PRIVATE ${LIBUSB_1_INCLUDE_DIRS} ${CMAKE_CURRENT_BINARY_DIR})
 target_compile_definitions(pok3rlib PRIVATE ${LIBUSB_1_DEFINITIONS})
 
 # Pok3rTool

proto_holtek.cpp
@@ -122,6 +122,10 @@ bool ProtoHoltek::getInfo(){
 
     // TODO Read Flash Security and Protection
 
+    // check status
+    zu32 count = getCmdStatus();
+    LOG("status: " << count);
+
     return true;
 }
 
@@ -189,6 +193,7 @@ bool ProtoHoltek::writeFirmware(const ZBinary &fwbinin){
     if(!rebootBootloader(true))
         return false;
 
+#if 0
     // clear option bytes
     if(!eraseFlash(0x1ff00000, 0x1ff00400)){
         return false;
@@ -202,6 +207,7 @@ bool ProtoHoltek::writeFirmware(const ZBinary &fwbinin){
             return false;
         }
     }
+#endif
 
     // erase pages
     if(!eraseFlash(0x0, 0xfbff)){ // HT32F1654
@@ -210,6 +216,12 @@ bool ProtoHoltek::writeFirmware(const ZBinary &fwbinin){
     }
 
     ZThread::sleep(ERASE_SLEEP);
+
+    // check status
+    zu32 count;
+    do {
+        count = getCmdStatus();
+    } while(count < 1);
 
     // Write firmware
     LOG("Write...");
@@ -275,6 +287,12 @@ bool ProtoHoltek::writeFlash(zu32 addr, ZBinary bin){
     arg.write(bin);
     if(!sendCmd(FLASH_CMD, FLASH_WRITE_SUBCMD, arg))
         return false;
+
+    zu32 count;
+    do {
+        count = getCmdStatus();
+    } while (count < 1);
+
     return true;
 }
 
@@ -378,4 +396,26 @@ bool ProtoHoltek::sendRecvCmd(zu8 cmd, zu8 subcmd, ZBinary &data){
 
     data.rewind();
     return true;
+}
+
+zu32 ProtoHoltek::getCmdStatus(){
+    ZBinary data(64);
+    data.fill(0);
+    int rc = dev->xferControl(0xa1, 0x01, 0x100, 0, data);
+    if(rc < 0){
+        ELOG("xferControl error");
+    }
+
+    DLOG("status:");
+    DLOG(ZLog::RAW << data.dumpBytes(4, 8));
+
+    // count number of success entries in status buffer
+    zu32 count = 0;
+    for (int i = 0; i < 64; i++) {
+        data.seek(i);
+        zu8 val = data.readu8();
+        if (val == 0x4f) count++;
+        if (val == 0x0) break;
+    }
+    return count;
 }

proto_holtek.h
@@ -91,6 +91,8 @@ private:
     //! Send command and recv response.
     bool sendRecvCmd(zu8 cmd, zu8 subcmd, ZBinary &data);
 
+    zu32 getCmdStatus();
+
 private:
     bool builtin;
     bool debug;

rawhid/hid.h
@@ -54,6 +54,7 @@ int rawhid_openall_filter(rawhid_filter_cb cb, void *user);
 
 int rawhid_recv(hid_t *hid, void *buf, int len, int timeout);
 int rawhid_send(hid_t *hid, const void *buf, int len, int timeout);
+int rawhid_xfer_control(hid_t *hid, unsigned char bmRequest, unsigned char bRequest, unsigned short wValue, unsigned short wIndex, void *buf, unsigned short wLength, int timeout);
 
 #ifdef __cplusplus
 }

rawhid/hid_libusb.c
@@ -121,6 +121,29 @@ int rawhid_send(hid_t *hid, const void *buf, int len, int timeout)
     }
 }
 
+//  rawhid_xfer_control - send and receive a control transfer
+//    Inputs:
+//	hid = device to transmit to
+//	bmRequest = request type
+//	bRequest = request
+//	wValue = value
+//	wIndex = Index
+//	buf = buffer containing packet to send
+//	len = number of bytes to transmit
+//	timeout = time to wait, in milliseconds
+//    Output:
+//	number of bytes sent, or -1 on error
+//
+int rawhid_xfer_control(hid_t *hid, unsigned char bmRequest, unsigned char bRequest, unsigned short wValue, unsigned short wIndex, void *buf, unsigned short wLength, int timeout)
+{
+    int rc;
+    if (!hid || !hid->open) return -1;
+    rc = libusb_control_transfer(hid->usb, bmRequest, bRequest, wValue, wIndex, (unsigned char *)buf, wLength, timeout);
+    if (rc == LIBUSB_ERROR_TIMEOUT) return 0;
+    if (rc < 0) return -1;
+    return rc;
+}
+
 struct hid_match {
     int max;
     int vid;

rawhid/hiddevice.h
@@ -30,6 +30,7 @@ public:
 
     bool send(const ZBinary &data, bool tolerate_dc = false);
     bool recv(ZBinary &data);
+    int xferControl(zu8 bmRequest, zu8 bRequest, zu16 wValue, zu16 wIndex, ZBinary &data);
 
     static ZArray<ZPointer<HIDDevice>> openAll(zu16 vid, zu16 pid, zu16 usage_page, zu16 usage);
 

commit 95f96e15494e2d5b6d2df3687cbe390564904c79

hiddevice: Fix macOS build
Fixes: 182e5cee8 "[HACK] proto_holtek: fix writeFirmware operation"

rawhid/hiddevice.cpp
@@ -90,6 +90,9 @@ bool HIDDevice::recv(ZBinary &data){
 }
 
 int HIDDevice::xferControl(zu8 bmRequest, zu8 bRequest, zu16 wValue, zu16 wIndex, ZBinary &data){
+#if LIBCHAOS_PLATFORM == LIBCHAOS_PLATFORM_MACOSX
+    return -1;
+#else
     if(!isOpen())
         return -1;
     if(data.size() == 0)
@@ -105,6 +108,7 @@ int HIDDevice::xferControl(zu8 bmRequest, zu8 bRequest, zu16 wValue, zu16 wIndex
         return ret;
     }
     return ret;
+#endif
 }
 
 ZArray<ZPointer<HIDDevice> > HIDDevice::openAll(zu16 vid, zu16 pid, zu16 usage_page, zu16 usage){